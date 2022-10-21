c9cc0a1329eb258909813eeae767d5475e2d7626
chris94440 <62375301+chris94440@users.noreply.github.com>
2022-10-21 10:01:14 +0200

    Update imaProtectNewAPI.class.php

--- 3rdparty/imaProtectNewAPI.class.php
+++ 3rdparty/imaProtectNewAPI.class.php
@@ -103,10 +103,17 @@ class imaProtectNewAPI {
       	log::add('alarme_IMA', 'debug', "				==> Response");
       	log::add('alarme_IMA', 'debug', "					# Code Http : $httpRespCode");
       	//log::add('alarme_IMA', 'debug', "					# Response  : ".$resultCurl);
-      	log::add('alarme_IMA', 'debug', "					# Body  : ".$body);
+      	if ($this->isJson($body)) {
+		log::add('alarme_IMA', 'debug', "					# Body  : ".$body);
+	}  
       	log::add('alarme_IMA', 'debug', "					# Header  : ".$header);
 		
 		return array($httpRespCode, $body, $header);
+	}
+	
+	private function isJson($inputJson) {
+	   json_decode($inputJson);
+	   return json_last_error() === JSON_ERROR_NONE;
 	}
 	
 	private function getCookiesFromGetRequest($header,$body) {
